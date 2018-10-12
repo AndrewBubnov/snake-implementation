1a1bcb56194db582ec4b55d34a5a74b890a904e8
AndrewBubnov <andrew.bubnov75@gmail.com>
2018-10-12 13:47:36 +0300

    Snake game implementation

--- js/main.js
+++ js/main.js
@@ -1,86 +1,63 @@
-
-// function Node(x,y) {
-//     this.x = x;
-//     this.y = y;
-//
-//     this.getOutNodes = function() {
-//        let newX;
-//        let newY;
-//        let nodesArray = [];
-//         newX = this.x - 1;
-//         if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
-//         newX = this.x + 1;
-//         if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
-//         newY = this.y - 1;
-//         if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
-//         newY = this.y + 1;
-//         if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
-//         return nodesArray;
-//     };
-//     this.equals = function (object) {
-//         return (this.x === object.x && this.y === object.y);
-//     }
-// }
 function Node(x,y) {
     this.x = x;
     this.y = y;
 
     this.getOutNodes = function(preference) {
-       let newX;
-       let newY;
-       let nodesArray = [];
-       if (preference === "x-y-") {
-           newX = this.x - 1;
-           if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
-           newY = this.y - 1;
-           if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
-           newX = this.x + 1;
-           if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
-           newY = this.y + 1;
-           if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
-           return nodesArray;
-       }
-       else if (preference === "x-y+") {
-           newX = this.x - 1;
-           if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
-           newY = this.y + 1;
-           if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
-           newY = this.y - 1;
-           if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
-           newX = this.x + 1;
-           if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
-           return nodesArray;
-       }
-       else if (preference === "x+y-") {
-           newY = this.y - 1;
-           if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
-           newX = this.x + 1;
-           if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
-           newX = this.x - 1;
-           if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
-           newY = this.y + 1;
-           if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
-           return nodesArray;
-       }
-       else {
-           newX = this.x + 1;
-           if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
-           newY = this.y + 1;
-           if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
-           newY = this.y - 1;
-           if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
-           newX = this.x - 1;
-           if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
-           return nodesArray;
-       }
+        let newX;
+        let newY;
+        let nodesArray = [];
+        if (preference === "x-y-") {
+            newX = this.x - 1;
+            if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
+            newY = this.y - 1;
+            if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
+            newX = this.x + 1;
+            if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
+            newY = this.y + 1;
+            if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
+            return nodesArray;
+        }
+        else if (preference === "x-y+") {
+            newX = this.x - 1;
+            if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
+            newY = this.y + 1;
+            if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
+            newY = this.y - 1;
+            if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
+            newX = this.x + 1;
+            if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
+            return nodesArray;
+        }
+        else if (preference === "x+y-") {
+            newY = this.y - 1;
+            if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
+            newX = this.x + 1;
+            if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
+            newX = this.x - 1;
+            if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
+            newY = this.y + 1;
+            if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
+            return nodesArray;
+        }
+        else {
+            newX = this.x + 1;
+            if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
+            newY = this.y + 1;
+            if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
+            newY = this.y - 1;
+            if (isAllowed(newY)) nodesArray.push(new Node(this.x, newY));
+            newX = this.x - 1;
+            if (isAllowed(newX)) nodesArray.push(new Node(newX, this.y));
+            return nodesArray;
+        }
     };
     this.equals = function (object) {
         return (this.x === object.x && this.y === object.y);
     }
 }
 Node.prototype.toString = function () {
-        return this.x + " ; " + this.y;
-    };
+    return this.x + " : " + this.y + " ";
+};
 
 function getPreference(start, finish) {
     let preference;
@@ -91,7 +68,7 @@ function getPreference(start, finish) {
     return preference;
 }
 function isAllowed(n) {
-   return (!(n < 1 || n > 20));
+    return (!(n < 1 || n > 20));
 }
 
 function getByKey (map, key) {
@@ -111,7 +88,7 @@ function hasKey(set, key) {
     return false;
 }
 
-let stopBody = false;
+
 function bfs(start,finish, stones) {
     let visited = new Set();
     let nodeStory = new Map();
@@ -137,11 +114,10 @@ function bfs(start,finish, stones) {
             }
         }
     }
-    // if (!current.equals(finish)) {
-    //     stopBody = true;
-    //     steps.push(new Node(-1, -1));
-    //     return steps;
-    // }
+    if (!current.equals(finish)) {
+        steps.push(new Node(-1, -1));
+        return steps;
+    }
     let node = finish;
     while(!node.equals(start)){
         steps.push(node);
@@ -162,97 +138,136 @@ let moves = [];
 let snakeBody = [];
 let interval;
 let snakeLength = 1;
+function indexOfElement(array, value) {
+    for (let i = 0; i < array.length; i++) {
+        if(array[i].equals(value)) return i;
+    }
+}
+
+let apple;
 function move(start, finish, stone) {
-        if (!headExists) {
-            $('<div>').addClass('head').appendTo($('#' + start.x + '_' + start.y));
-            headExists = true;
-        }
+    if (!headExists) {
+        $('<div>').addClass('head').appendTo($('#' + start.x + '_' + start.y));
+        headExists = true;
+    }
+
         interval = setInterval(function () {
-            let temp = [start.x, start.y];
-            moves.push(temp);
-            if (moves.length > 200) {
-                moves = moves.slice(100, 202);
-            }
-            let nextStep = bfs(start, finish, stone)[0];
+
+        moves.push(start);
+        if (moves.length > 200) {
+            moves = moves.slice(100, 202);
+        }
+        let steps = bfs(start, finish, stone);
 
 
+        let tail = moves.concat(steps[0]);
 
-//**********************************************************************************************
+        let tempStone = tail.slice(tail.length - snakeLength + 1);
+        tempStone.push(stone[0]);
 
-// if (nextStep.equals(new Node(-1, -1))){
-//     moves.pop();
-//     start = new Node(moves[moves.length - 1][0], moves[moves.length - 1][1]);
-//         nextStep = allowedPlace(start);
-//         moves.push([nextStep.x, nextStep.y]);
-//         stopBody = false;
-// }
+        let tempSearch;
 
+        if (snakeLength > 1){
+            if (snakeBody.length > 0 && steps[0].x === - 1 && steps[0].y === - 1){
 
-//***************************************************************************************************
+                console.log("can't reach apple, going after tail");
+                finish = new Node(snakeBody[snakeBody.length - 1].x, snakeBody[snakeBody.length - 1].y);
+                let array = snakeBody.slice(0, snakeBody.length - 2);
+                array.push(stone[0]);
+                steps = bfs(start, finish, array);
 
+            } else {
+                tempSearch = bfs(finish, tail[tail.length - snakeLength], tempStone);
 
+                if (tempSearch[0].x === -1 && tempSearch[0].y === -1){
+                    console.log("can't reach tail after reaching apple, going after tail");
 
-            let closed = snakeBody.some(function (element) {
-                return nextStep.x === element[0] && nextStep.y === element[1];
-            });
+                    finish = new Node(snakeBody[snakeBody.length - 1].x, snakeBody[snakeBody.length - 1].y);
+                    let array = snakeBody.slice();
+                    array.splice(-2, 2).push(stone[0]);
+                    steps = bfs(start, finish, array);
 
-            if (closed){
-                let len = snakeBody.length > 1 ? snakeBody.length - 1 : snakeBody.length;
-                for (let j = 0; j < len; j++) {
-                    stone.push(new Node(snakeBody[j][0], snakeBody[j][1]));
+                } else {
+                    console.log("plain search for the apple");
                 }
-                nextStep = bfs(start, finish, stone)[0];
+
             }
+        }
 
 
-            $('.head').appendTo('#' + nextStep.x + '_' + nextStep.y);
-            if (nextStep.x < start.x) $('.head').removeClass('turnUp').removeClass('turnDown').addClass('flipLeft');
-            else if (nextStep.y < start.y) $('.head').removeClass('flipLeft').removeClass('turnDown').addClass('turnUp');
-            else if (nextStep.y > start.y) $('.head').removeClass('flipLeft').removeClass('flipLeft').addClass('turnDown');
-            else $('.head').removeClass('flipLeft').removeClass('turnUp').removeClass('turnDown');
-            if (snakeBody.length > 0 && !stopBody){
-                for (let i = 0; i < snakeBody.length; i++) {
-                    let currentStep = moves.length - (i + 1);
-                    snakeBody[i][0] = moves[currentStep][0];
-                    snakeBody[i][1] = moves[currentStep][1];
-                    $('#' + (i + 1)).appendTo('#' + snakeBody[i][0] + '_' + snakeBody[i][1]);
-                }
+        let nextStep = steps[0];
+
+
+
+        $('.head').appendTo('#' + nextStep.x + '_' + nextStep.y);
+        if (nextStep.x < start.x) $('.head').removeClass('turnUp').removeClass('turnDown').addClass('flipLeft');
+        else if (nextStep.y < start.y) $('.head').removeClass('flipLeft').removeClass('turnDown').addClass('turnUp');
+        else if (nextStep.y > start.y) $('.head').removeClass('flipLeft').removeClass('flipLeft').addClass('turnDown');
+        else $('.head').removeClass('flipLeft').removeClass('turnUp').removeClass('turnDown');
+        if (snakeBody.length > 0){
+            for (let i = 0; i < snakeBody.length; i++) {
+                let currentStep = moves.length - (i + 1);
+                snakeBody[i].x = moves[currentStep].x;
+                snakeBody[i].y = moves[currentStep].y;
+                $('#' + (i + 1)).appendTo('#' + snakeBody[i].x + '_' + snakeBody[i].y);
             }
-            start = nextStep;
-            currentHead = nextStep;
-            if (start.equals(finish)) {
-                $('.apple').remove();
-                // $('.stone').remove();
+        }
+        start = nextStep;
+        currentHead = nextStep;
+        if (start.equals(finish)) {
+
+            if ($('#' + finish.x + '_' + finish.y).children().first().hasClass('apple')){
+            $('.apple').remove();
+
+            clearInterval(interval);
+
+            let bodyX = moves[moves.length - snakeLength].x;
+            let bodyY = moves[moves.length - snakeLength].y;
+
+            $('<div>').addClass('snake').attr('id', snakeLength).appendTo($('#' + bodyX + '_' + bodyY));
+
+
+
+            $('.head').addClass('green');
+            $('.snake').addClass('green');
+            setTimeout(function () {
+                $('.head').removeClass('green');
+                $('.snake').removeClass('green');
+            }, 200);
+            $('.score').text(snakeLength);
+            snakeLength++;
+            snakeBody.push(new Node(bodyX, bodyY));
+
+            main(start);
+            } else {
+
                 clearInterval(interval);
 
-                let bodyX = moves[moves.length - snakeLength][0];
-                let bodyY = moves[moves.length - snakeLength][1];
+                let bodyX = moves[moves.length - snakeLength].x;
+                let bodyY = moves[moves.length - snakeLength].y;
+                console.log('line 307: finish(start) = ' + finish);
+                console.log('line 307: apple = ' + apple);
+                console.log('line 307: BFS = ' + bfs(finish, apple, stone));
 
-                $('<div>').addClass('snake').attr('id', snakeLength).appendTo($('#' + bodyX + '_' + bodyY));
-                $('.head').addClass('green');
-                $('.snake').addClass('green');
-                setTimeout(function () {
-                    $('.head').removeClass('green');
-                    $('.snake').removeClass('green');
-                }, 200);
-                $('.score').text(snakeLength);
-                snakeLength++;
-                snakeBody.push([bodyX, bodyY]);
-
-                main(start);
+                if (bfs(finish, apple, stone)[0] !== -1){
+                    move(finish, apple, stone);
+                } else {
+                    move(finish, new Node(snakeBody[snakeBody.length - 1].x, snakeBody[snakeBody.length - 1].y), stone);
+                }
             }
-     }, 100);
+        }
+    }, 100);
 }
 
-function allowedPlace(start) {
-    let node;
-    if (start.x < 20 && !restrictedLocked(start.x + 1, start.y)) node = new Node(start.x + 1, start.y);
-    else if (start.x > 1 && !restrictedLocked(start.x - 1, start.y)) node = new Node(start.x - 1, start.y);
-    else if (start.y < 20 && !restrictedLocked(start.x, start.y + 1)) node = new Node(start.x, start.y + 1);
-    else if (start.y > 1 && !restrictedLocked(start.x, start.y - 1))node = new Node(start.x, start.y - 1);
-    else node = start;
-    return node;
-}
+// function allowedPlace(start) {
+//     let node;
+//     if (start.x < 20 && !restrictedLocked(start.x + 1, start.y)) node = new Node(start.x + 1, start.y);
+//     else if (start.x > 1 && !restrictedLocked(start.x - 1, start.y)) node = new Node(start.x - 1, start.y);
+//     else if (start.y < 20 && !restrictedLocked(start.x, start.y + 1)) node = new Node(start.x, start.y + 1);
+//     else if (start.y > 1 && !restrictedLocked(start.x, start.y - 1))node = new Node(start.x, start.y - 1);
+//     else node = start;
+//     return node;
+// }
 
 
 // function defineDirection(moves, currentStep) {
@@ -277,6 +292,7 @@ function main(start) {
         }
     }
     stoneArray = Array.from(stoneSet);
+    stoneArray = stoneArray.concat(snakeBody);
     let appleX;
     let appleY;
     while(true) {
@@ -285,7 +301,7 @@ function main(start) {
         if (!restrictedPlace(appleX, appleY, start)) break;
     }
     $('<div>').addClass('apple').appendTo($('#' + appleX + '_' + appleY));
-
+    apple = new Node(appleX, appleY);
     move(currentHead, new Node(appleX, appleY), stoneArray);
 }
 
@@ -298,27 +314,20 @@ function restrictedPlace(x, y, start) {
     }) || (start.x === x && start.y === y);
 }
 
-function restrictedLocked(x, y) {
-    return stoneArray.some(function (element) {
-        return element.x === x && element.y === y;
-    }) || snakeBody.some(function (element) {
-        return element[0] === x && element[1] === y;
-    });
-}
+// function restrictedLocked(x, y) {
+//     return stoneArray.some(function (element) {
+//         return element.x === x && element.y === y;
+//     }) || snakeBody.some(function (element) {
+//         return element[0] === x && element[1] === y;
+//     });
+// }
 
 
 $('.button-container').on('click', function (e) {
-    console.log(e.target.id);
     if (e.target.id === 'start'){
-    main();
+        main();
     }
     if (e.target.id === 'stop'){
-    clearInterval(interval);
+        clearInterval(interval);
     }
 });
-
-
-
-
-
-
